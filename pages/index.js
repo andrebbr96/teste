3be32a88ef8aa673e9d2d93035b9b9e503f33725
andrebbr96 <80175371+andrebbr96@users.noreply.github.com>
2023-10-08 18:44:08 +0000

    adiciona scripts  `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Construindo meu futuro com a pessoa mais especial para mim, Livia Zivah Cristina !</h1>   
+function Home() {
+  return (
+    <h1>
+      Construindo meu futuro com a pessoa mais especial para mim, Livia Zivah
+      Cristina !
+    </h1>
+  );
 }
 
 export default Home;
